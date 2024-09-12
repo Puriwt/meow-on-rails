fbfc0f310cd5c93ff0ba8486ea226c768ee3b404
Puriwt <111586756+Puriwt@users.noreply.github.com>
2024-09-12 23:51:25 +0700

    To validate email and birthdate to valid input.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :birthdate, :gender, :email, :phone, :subject, presence: true
   validates :phone, length: { minimum: 12, maximum: 12 }
+  validate :valid_birthdate
+  validate :valid_email
+
+  def valid_birthdate
+    if birthdate.present? && birthdate > Date.today
+      errors.add(:birthdate, "can't be in the future")
+    end
+  end
+
+  def valid_email
+    if email.present? && !email.match?(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+      errors.add(:email, "is incorrect")
+    end
+  end
 
   def self.search(query)
     if query.present?
